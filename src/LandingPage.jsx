4b8ef765bbf6f8dab8f1b483c8e4444a2e96f3d7
Tawana233 <2588206@students.wits.ac.za>
2025-04-10 15:30:38 +0200

    Added Landing Page

---
+++ src/LandingPage.jsx
@@ -0,0 +1,38 @@
+import "./LandingPage.css";
+
+const LandingPage = () => {
+
+    return(
+    <main className="LandingPage-main">
+
+      <section className="LandingPage-HeaderBar">
+        <header className="LandingPage-HeaderLeft"><h2>FacilityPro</h2></header>
+        <header className="LandingPage-HeaderRight">
+        <ul className="nav-links">
+          <li><a className="nav-links a" href = "">Documentation</a></li>
+          <li><a className="nav-links a" href = "">Terms</a></li>
+          <li><a className= "nav-links a" href = "">Privacy</a></li>
+        </ul>
+        </header>
+      </section>
+      
+      <section className="LandingPage-Middle">
+      <section className="LandingPage-LeftSection">
+        <h2 className="LandingPage-text">Facility Pro let's you schedule and manage your local facilities, Hassle Free.</h2>
+        <p className="LandingPage-text">We want to see communities outside again. Plan sports days, without the headache.</p>
+      </section>
+
+      <section className="LandingPage-RightSection">
+        <section className="LandingPage-SignLogContainer">
+        <button id="SignIn Button">Sign In with Google</button>
+        <p>Don't have an account?</p>
+        <button id="SignUp Button">Sign Up with Google</button>
+        </section>
+      </section>
+      </section>
+    </main>
+
+    );
+}
+
+export default LandingPage;
