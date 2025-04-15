1e1581ec9fc0d586b4f3ed863b045cc74a2e79ca
thembu <rakhivhanithembu@gmail.com>
2025-04-16 00:45:41 +0200

    added backend code to update issues and made the page for showing issue information

--- backend/firebase/firebase.config.js
+++ backend/firebase/firebase.config.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAnalytics, isSupported } from "firebase/analytics";
-
+import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -20,7 +20,7 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const analytics = getAnalytics(app);
-
+export const storage = getStorage(app);
 
 if (process.env.NODE_ENV !== 'test') {
   isSupported().then((supported) => {
