d06ae77146ec53b14bba90f6b28ae61dc59fee0b
Tawana233 <2588206@students.wits.ac.za>
2025-04-15 09:55:05 +0200

    Add Very Basic Issues Functionality

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import SignupPage from './components/SignupPage.jsx';
 import ResidentDashboard from './components/ResidentDashboard.jsx';  // 
 import AdminDashboard from './components/AdminDashboard.jsx';
 import FacilityStaffDashboard from './components/FacilityStaffDashboard.jsx'
+import IssuesPage from './components/IssuesPage.jsx';
 
 function App() {
   return (
@@ -17,7 +18,7 @@ function App() {
         <Route path="/SignupPage" element={<SignupPage />} /> 
         <Route path="/resident-home" element={<ResidentDashboard />} />
         <Route path ="/admin-home" element={<AdminDashboard />} />
-        <Route path ="/staff-home" element={<FacilityStaffDashboard />} />
+        <Route path ="/staff-home" element={<IssuesPage />} />
 
       </Routes>
     </Router>
