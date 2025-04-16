8df61ca01990633b048b8028fc02e2b316707dfc
thembu <rakhivhanithembu@gmail.com>
2025-04-16 15:59:59 +0200

    addedd additional states for maintainence status

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,28 @@
-import React from 'react';  
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';  // Import necessary routing components
-import LandingPage from './components/LandingPage';
-import LoginPage from './components/LoginPage.jsx'; 
-import SignupPage from './components/SignupPage.jsx'; 
-import ResidentDashboard from './components/resident/ResidentDashboard.jsx';  //
-import AdminDashboard from './components/admin/AdminDashboard.jsx';
-import FacilityStaffDashboard from './components/staff/FacilityStaffDashboard.jsx'
-import IssuesPage from './components/staff/IssuesPage.jsx';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Import necessary routing components
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import LandingPage from "./components/LandingPage";
+import LoginPage from "./components/LoginPage.jsx";
+import SignupPage from "./components/SignupPage.jsx";
+import ResidentDashboard from "./components/resident/ResidentDashboard.jsx"; //
+import AdminDashboard from "./components/admin/AdminDashboard.jsx";
+import FacilityStaffDashboard from "./components/staff/FacilityStaffDashboard.jsx";
+import IssuesPage from "./components/staff/IssuesPage.jsx";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        
-        <Route path="/" element={<LandingPage />} />  
-        <Route path="/LoginPage" element={<LoginPage />} /> 
-        <Route path="/SignupPage" element={<SignupPage />} /> 
-        <Route path="/resident-home" element={<ResidentDashboard />} />
-        <Route path ="/admin-home" element={<AdminDashboard />} />
-        <Route path ="/staff-home" element={<FacilityStaffDashboard />} />
-        <Route path ="/staff-issues" element={<IssuesPage />} />
+      <ToastContainer />
 
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/LoginPage" element={<LoginPage />} />
+        <Route path="/SignupPage" element={<SignupPage />} />
+        <Route path="/resident-home" element={<ResidentDashboard />} />
+        <Route path="/admin-home" element={<AdminDashboard />} />
+        <Route path="/staff-home" element={<FacilityStaffDashboard />} />
+        <Route path="/staff-issues" element={<IssuesPage />} />
       </Routes>
     </Router>
   );
