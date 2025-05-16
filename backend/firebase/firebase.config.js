9ea7cec1736c724786029392477a5a29dcdc8cf5
Austin Mthombeni <2586407@students.wits.ac.za>
2025-05-16 19:40:07 +0200

    added enviroment viables

--- backend/firebase/firebase.config.js
+++ backend/firebase/firebase.config.js
@@ -1,33 +1,26 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAnalytics, isSupported } from "firebase/analytics";
 import { getStorage } from "firebase/storage";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC1CymHohELYGN7EipJOC5sL9iQe5sIOzc",
-  authDomain: "facilty-pro.firebaseapp.com",
-  projectId: "facilty-pro",
-  storageBucket: "facilty-pro.firebasestorage.app",
-  messagingSenderId: "33531147470",
-  appId: "1:33531147470:web:46c8bc113fdf63a5a4cc9f",
-  measurementId: "G-89EB162VPS"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-export const analytics = getAnalytics(app);
 export const storage = getStorage(app);
 
-if (process.env.NODE_ENV !== 'test') {
-  isSupported().then((supported) => {
-    if (supported) {
-      getAnalytics(app);
-    }
-  }).catch((e) => {
-    console.warn("Analytics not supported:", e);
-  });
-}
+isSupported().then((supported) => {
+  if (supported) {
+    getAnalytics(app);
+  }
+}).catch((e) => {
+  console.warn("Analytics not supported:", e);
+});
